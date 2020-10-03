bf439cc18b33ee2148143268f3ba780de36d557e
akhil30802 <72270079+akhil30802@users.noreply.github.com>
2020-10-03 08:36:53 +0530

    Update Simple Interest.py

--- Simple Interest.py
+++ Simple Interest.py
@@ -10,8 +10,31 @@ class simpleinterest:
       cal = mul/100
       fres = str(cal)
       print("Simple Interest (SI)= (P*T*R)/100")
-      print("                 SI = ("+ p + " * "+ t + " * " + r + ") / 1000")
+      print("                 SI = ("+ p + " * "+ t + " * " + r + ") / 100")
       print("                 SI = (" + res + ") / 100")
       print("                 SI = " + fres)
       print("Therefore, SI = "+ fres)
         
+
+            
+            
+            
+            
+            
+            
+# short code
+
+p = float(input("Enter Principal(P): "))             # asking for principal
+t = float(input("Enter Rate(R): "))                  # asking for rate
+r = float(input("Enter Time(T): "))                  # asking for time
+
+
+print("Simple Interest (SI)= (P*T*R)/100")
+
+print("                 SI = (", str(p) ," * ", str(t)  ," * ",  str(r),  ") / 100" )
+
+print("                 SI = (", p * t * r,") / 100" )
+
+print("                 SI = " ,(p*r*t)/100)
+print("Therefore, SI = " , (p*r*t)/100)
+
